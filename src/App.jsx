92c5e7e0f3a59e49d91d2a882385821ec6599d0e
Huzaifa Gulzar <huzaifa6715@gmail.com>
2025-02-14 09:55:10 +0500

    add Tailwind CSS integration and update App component styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+       Hello world!
+      </h1>
+    </div>
   )
 }
 
